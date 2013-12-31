3dab9bda00cab4123caf368443318ca4d2fcefc6
George Bryant <gb@gbryant.co.uk>
2013-12-31 23:33:40 +0000

    Added terrainGen.py with skeleton project

---
+++ pyterraingen/terrainGen.py
@@ -0,0 +1,40 @@
+"""
+pyTerainGen - 2D terrain generator in Python
+
+Copyright (C) 2014  George Bryant
+
+This program is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+You should have received a copy of the GNU General Public License along with
+    this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import random
+
+
+if __name__ == "__main__":
+    print(random.randrange(0, 100))
+
+    def getParameters():
+        pass
+    
+    def loop(length, maxHeight):
+        pass
+
+    def rng():
+        pass
+
+    def randAnalyser(randNum, lo, hi):
+        pass
+
+    def printTerrain(heightArray):
+        pass
